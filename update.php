bdacb11cc91941bc5ccb05c671cc51a61b208550
ProBatou <root@probatou.com>
2022-01-21 18:36:54 +0100

    remove // update.php

--- update.php
+++ update.php
@@ -7,10 +7,10 @@ function run_update() {
     $versiongithub = exec('curl --silent -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/ProBatou/WebApp/releases/latest | grep -oP '. "'(?<=".'"tag_name": ")[^"]*'."' | sed 's/^v//'");
 
     exec('wget https://github.com/ProBatou/WebApp/archive/refs/tags/v'.$versiongithub.'.tar.gz');
-    //exec('tar -xvf *'.$versiongithub.'.tar.gz --strip 1 && rm *'.$versiongithub.'.tar.gz');
-    //exec("sed -i 's/" . $version . "/" . $versiongithub . "/g' " . $file); 
+    exec('tar -xvf *'.$versiongithub.'.tar.gz --strip 1 && rm *'.$versiongithub.'.tar.gz');
+    exec("sed -i 's/" . $version . "/" . $versiongithub . "/g' " . $file); 
 
-    //file_put_contents($file, $versiongithub);
+    file_put_contents($file, $versiongithub);
 }
 run_update();
 
