5c8fd4903592d6a74833be6a08ddc8d15c1d87d1
ProBatou <baptiste2105@hotmail.fr>
2022-04-18 01:45:32 +0200

    remove verbose

--- update.php
+++ update.php
@@ -7,7 +7,7 @@ function run_update() {
     $versiongithub = exec('curl --silent -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/ProBatou/WebApp/releases/latest | grep -oP '. "'(?<=".'"tag_name": ")[^"]*'."' | sed 's/^v//'");
 
     exec('wget https://github.com/ProBatou/WebApp/archive/refs/tags/v'.$versiongithub.'.tar.gz');
-    exec('tar -xvf *'.$versiongithub.'.tar.gz --strip 1 && rm *'.$versiongithub.'.tar.gz');
+    exec('tar -xf *'.$versiongithub.'.tar.gz --strip 1 && rm *'.$versiongithub.'.tar.gz');
 
     file_put_contents($file, $versiongithub);
 }
