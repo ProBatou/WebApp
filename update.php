d23d450f0b0cc258d69309d3e818957a581d0dbc
ProBatou <baptiste2105@hotmail.fr>
2022-11-21 18:08:36 +0100

    Auto refresh after update

--- update.php
+++ update.php
@@ -3,14 +3,14 @@
 function run_update() {
 
     $file = 'version.txt';
-    $version = file_get_contents($file);
     $versiongithub = exec('curl --silent -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/ProBatou/WebApp/releases/latest | grep -oP '. "'(?<=".'"tag_name": ")[^"]*'."' | sed 's/^v//'");
 
     exec('wget https://github.com/ProBatou/WebApp/archive/refs/tags/v'.$versiongithub.'.tar.gz');
     exec('tar -xf *'.$versiongithub.'.tar.gz --strip 1 && rm *'.$versiongithub.'.tar.gz');
-
+    
     file_put_contents($file, $versiongithub);
 }
+
 run_update();
 
 ?>
